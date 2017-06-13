b2c1f1564f0153f7688b1b78d3aa8c1401fd24dd
MASHA <mashanikit@gmail.com>
2017-06-13 15:13:03 -0400

    changed party guests table to widgets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613185618) do
+ActiveRecord::Schema.define(version: 20170613190738) do
 
   create_table "locations", force: :cascade do |t|
     t.string  "name"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20170613185618) do
     t.integer "quantity"
   end
 
-  create_table "party_guests", force: :cascade do |t|
+  create_table "widgets", force: :cascade do |t|
     t.string  "first_name"
     t.string  "last_name"
     t.string  "dietary_restrictions"
